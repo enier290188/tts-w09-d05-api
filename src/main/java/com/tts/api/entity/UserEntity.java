d14a88336a83e8b86ed994e905310c7ff8154744
Enier Ramos García <enier290188@gmail.com>
2020-12-18 15:39:58 -0500

    1. In the User class, add the following annotation-based validation:     - First name must be a maximum of 20 characters in length     - Last name must be a minimum of 2 characters in length     - State must be between 4 and 20 characters in length

--- src/main/java/com/tts/api/entity/UserEntity.java
+++ src/main/java/com/tts/api/entity/UserEntity.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -20,8 +21,11 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 0, max = 20)
     private String firstName;
+    @Size(min = 2, max = 20)
     private String lastName;
+    @Size(min = 4, max = 20)
     private String state;
 
     public UserEntity(String firstName, String lastName, String state) {
